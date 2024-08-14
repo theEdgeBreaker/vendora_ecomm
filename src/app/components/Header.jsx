869ceae6e74de9dacfdc2bc42ffffaa19ed18bad
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-14 13:40:56 +0530

    update heading

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="bg-blue-600 text-white p-4">
       <nav className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">My E-commerce App</h1>
+        <h1 className="text-xl font-bold">Vendora</h1>
         <ul className="flex space-x-4">
           <li>
             <Link className="hover:underline" href="/">
