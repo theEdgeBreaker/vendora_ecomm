56606cacea030e93f6a64c363963f6df4ddce6ac
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-14 13:39:57 +0530

    Add Header and Footer components to layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,5 @@
 import Image from "next/image";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-      Hello World
-      <Footer />
-    </div>
-  );
+  return <div>Hello World</div>;
 }
