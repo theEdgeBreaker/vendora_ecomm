b3dbc39559a85bfdcb5b250ea3015353b9d357d7
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-14 13:31:24 +0530

    Add Header component

---
+++ src/app/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-bold">My E-commerce App</h1>
+        <ul className="flex space-x-4">
+          <li>
+            <Link className="hover:underline" href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:underline" href="/products">
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link className="hover:underline" href="/about">
+              About
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Header from "./components/Header";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      Hello World
+    </div>
+  );
 }
