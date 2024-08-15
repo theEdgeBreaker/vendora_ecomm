db6cf7a42e9c1ae26e569cf72e0a2e4b26e1ff08
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-15 11:52:24 +0530

    feat: improve alignment of product details and add hover effect

--- src/app/components/products/ProductList.jsx
+++ src/app/components/products/ProductList.jsx
@@ -4,41 +4,38 @@ import { products } from "../../../data/products";
 
 const ProductList = () => {
   return (
-    // <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 px-14 py-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 px-20 py-6">
       {products.map((product) => (
         <div
           key={product.id}
-          //   className="border rounded-lg p-4 shadow hover:shadow-lg transition"
-          className=" rounded-sm px-2 py-5 hover:shadow-lg transition "
+          className="relative group rounded-sm p-2 overflow-hidden hover:shadow-lg transition"
         >
-          <div className="relative w-full h-48 mb-4">
+          <div className="relative w-full h-48">
             <Image
               src={product.image}
               alt={product.name}
               fill
-              //   className="object-cover rounded-md"
-              className="object-cover rounded-sm"
+              className="object-cover rounded-t-sm"
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
           </div>
-          <p className="text-sm mb-2 text-gray-700 overflow-hidden whitespace-nowrap text-ellipsis">
-            {product.description}
-          </p>
-          <p className=" text-xs mb-2 text-gray-400">{product.feature}</p>
-          <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-          <p className="text-lg font-medium text-gray-800 mb-4">
-            {new Intl.NumberFormat("en-US", {
-              style: "currency",
-              currency: "USD",
-            }).format(product.price)}
-          </p>
-          <button
-            //   className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-sm hover:bg-blue-600 transition"
-          >
-            Add to Cart
-          </button>
+
+          <div className="pt-4 transition-transform transform group-hover:-translate-y-4 ">
+            <p className="text-sm mb-2 text-gray-700 overflow-hidden whitespace-nowrap text-ellipsis">
+              {product.description}
+            </p>
+            <p className="text-xs mb-2 text-gray-400">{product.feature}</p>
+            <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+            <p className="text-lg font-medium text-gray-800 mb-4">
+              {new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(product.price)}
+            </p>
+            <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-sm hover:bg-blue-600 transition">
+              Add to Cart
+            </button>
+          </div>
         </div>
       ))}
     </div>
