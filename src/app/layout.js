551ceace185b0af76e67914cb79be779760c42d4
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-16 11:41:17 +0530

    feat: integrate CartProvider into RootLayout for global cart state management

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { cartProvider } from "../context/cartContext";
 
 export const metadata = {
   title: "Vendora",
@@ -12,9 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-100 flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
+        <cartProvider>
+          <Header />
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </cartProvider>
       </body>
     </html>
   );
