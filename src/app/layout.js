5c74ab3613f810411a837ecf84c0f1f1f7d5b88c
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-16 13:02:39 +0530

    Fix component name from <cartProvider> to <CartProvider> in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { cartProvider } from "../context/cartContext";
+import { CartProvider } from "../context/cartContext";
 
 export const metadata = {
   title: "Vendora",
@@ -13,11 +13,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-gray-100 flex flex-col min-h-screen">
-        <cartProvider>
+        <CartProvider>
           <Header />
           <main className="flex-grow">{children}</main>
           <Footer />
-        </cartProvider>
+        </CartProvider>
       </body>
     </html>
   );
