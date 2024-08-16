28656f5ae815fb059a3cb4e2e5acb4925df2ae6d
theEdgeBreaker <thesanjana2002@gmail.com>
2024-08-16 10:38:09 +0530

    feat(context): set up CartContext with basic state management

---
+++ src/context/cartUtils.js
@@ -0,0 +1,11 @@
+import React, { createContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <CartContext.Provider value={{ cart }}>{children}</CartContext.Provider>
+  );
+};
